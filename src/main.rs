0d641a404672cee3043be02cfdcdfc5682c0ea4b
Luigi Pocay <lpocay@w3itsolutions.net>
2023-12-12 16:07:27 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use std::process::Command;
+
+fn main() {
+    let out = Command::new("git").arg("tag").output().expect("Error");
+    println!("Status: {}", out.status);
+    println!("Content: {}", String::from_utf8_lossy(&out.stdout));
+}
